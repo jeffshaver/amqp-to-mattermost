2ed5143c59ab5b5c909413f9ef4060046190ea47
Jeffrey Shaver <jeff.e.shaver@gmail.com>
2018-01-19 15:06:02 -0500

    ReconnectingAMQP was extracted into a separate module. Switch to using that

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ process.on('unhandledException', e => {
 const fs = require('fs')
 const { URL } = require('url')
 const { logger } = require('./logger')
-const { ReconnectingAMQP } = require('./recconecting-amqp')
+const { ReconnectingAMQP } = require('reconnecting-amqp')
 const {
   AMQP_CA_CERT_LOCATION,
   AMQP_CERT_KEY_LOCATION,
@@ -104,30 +104,38 @@ async function start() {
   try {
     await amqp.connect()
 
-    amqp.consume(QUEUE, message => {
-      const body = JSON.stringify(
-        {
-          username: 'alert-bot',
-          text: JSON.stringify(JSON.parse(message.content.toString()), null, 2)
-        },
-        null,
-        2
-      )
-      const req = request({
-        ...connectionOptions,
-        headers: {
-          'Content-Type': 'application/json',
-          'Content-Length': body.length
-        }
-      })
+    amqp.consume(
+      QUEUE,
+      message => {
+        const body = JSON.stringify(
+          {
+            username: 'alert-bot',
+            text: JSON.stringify(
+              JSON.parse(message.content.toString()),
+              null,
+              2
+            )
+          },
+          null,
+          2
+        )
+        const req = request({
+          ...connectionOptions,
+          headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': body.length
+          }
+        })
 
-      req.on('error', e => {
-        logger.error(`Problem with request: ${e.message}`)
-      })
+        req.on('error', e => {
+          logger.error(`Problem with request: ${e.message}`)
+        })
 
-      req.write(body)
-      req.end()
-    })
+        req.write(body)
+        req.end()
+      },
+      true
+    )
   } catch (e) {
     logger.warn(e)
   }
